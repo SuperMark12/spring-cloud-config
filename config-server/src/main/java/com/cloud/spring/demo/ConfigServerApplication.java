a364c7d6b7b081b581220a27d10623501777dac6
doudou <763778255@qq.com>
2019-10-17 14:25:54 +0800

    config server提交

---
+++ config-server/src/main/java/com/cloud/spring/demo/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.cloud.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description:
+ * @author: Administrator
+ * @date: 2019/10/17
+ * @modified by:
+ * @modified date:
+ * @problem no:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cloud/spring/demo/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
